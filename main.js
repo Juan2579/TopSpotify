3ed943d50d708d9556ce92cda37ce95899d5fd33
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-01 17:29:52 -0500

    Initial design of navbar mobile and album section finished

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-const API = "https://spotify-scraper.p.rapidapi.com/v1/chart/tracks/top"
+const API = "https://spotify-scraper.p.rapidapi.com/v1/chart"
+
 const songsContainer = document.querySelector(".songs_content")
+
 const songArtists = document.querySelector(".song_artists")
+
+const albumsContainer = document.querySelector(".albums_content")
+
+//navbar
+const menuHamburger = document.querySelector(".navbar_menu")
+const menuClose = document.querySelector(".navbar_close")
+const menuHidden = document.querySelector(".navbar_mobile-hidden")
+
+const openNavigation = () => {
+    menuHamburger.style.display = "none"
+
+    menuClose.style.display = "block"
+
+    menuHidden.classList.add("shown")
+    menuHidden.classList.remove("hidden")
+
+}
+const closeNavigation = () => {
+    menuHamburger.style.display = "block"
+
+    menuClose.style.display = "none"
+
+    menuHidden.classList.remove("shown")
+    menuHidden.classList.add("hidden")
+    
+}
+//end of navbar
 const options = {
 	method: 'GET',
 	headers: {
@@ -17,22 +46,51 @@ const fetchData = async (urlApi) => {
 
 async function songs(){
     try {
-        const tracks = await fetchData(API)
+        const tracks = await fetchData(`${API}/tracks/top`)
         let count = 1
-        const listSongs = tracks.tracks
+        const listSongs = tracks?.tracks
         
             let templateSongs = `${listSongs.map(song => 
                 `<div class="song_card">
                     <p class="song_name">${count++}. ${song.name}</p>
                     <img src="${song.album.cover[0].url}" alt="">
-                    <p class="song_artists">${song.artists.map(artist => artist.name).join(', ')}</p>
+                    <p class="song_artists">${song.artists?.map(artist => artist.name).join(', ')}</p>
                 </div>`).slice(0,10).join("")
                 }`
         songsContainer.innerHTML = templateSongs    
     } catch (error) {
         console.log(error)
     }
-
 }
 
-songs()
+async function albums(){
+    
+    try {
+        const albums = await fetchData(`${API}/albums/top`)
+        let count = 1
+        const listAlbums = albums?.albums
+
+        let templateAlbums = `${listAlbums.map(album => 
+            `<div class="album_card">
+                <p class="album_name">${count++}. ${album?.name}</p>
+                <img src="${album?.cover[0].url}"/>
+                <p class="album_artist">${album?.artists[0].name}</p>
+
+            </div>`).slice(0,10).join("")
+
+        }`
+        albumsContainer.innerHTML = templateAlbums
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+async function create(){
+    setTimeout(
+        await songs(), 1000
+    )
+    await albums()
+}
+create()
+
