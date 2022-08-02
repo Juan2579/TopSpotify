00e092957fae7d306c60ae3093d934de1463e825
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-02 11:38:07 -0500

    Added artists section with its functionality

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+//api variables
 const API = "https://spotify-scraper.p.rapidapi.com/v1/chart"
+const APIartists = "https://spotify23.p.rapidapi.com/artists/?ids="
 
+const optionsArtists = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c723e9296dmshc409b9d1e1ad335p1c4721jsnac0711b1ef90',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+const optionsMain = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c723e9296dmshc409b9d1e1ad335p1c4721jsnac0711b1ef90',
+		'X-RapidAPI-Host': 'spotify-scraper.p.rapidapi.com'
+	}
+};
+//dom
 const songsContainer = document.querySelector(".songs_content")
 
 const songArtists = document.querySelector(".song_artists")
 
 const albumsContainer = document.querySelector(".albums_content")
+
+const artistsContainer = document.querySelector(".artists_content")
 
 //navbar
 const menuHamburger = document.querySelector(".navbar_menu")
@@ -30,13 +49,7 @@ const closeNavigation = () => {
     
 }
 //end of navbar
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c723e9296dmshc409b9d1e1ad335p1c4721jsnac0711b1ef90',
-		'X-RapidAPI-Host': 'spotify-scraper.p.rapidapi.com'
-	}
-};
+
 
 // const fetchData = async (urlApi) => {
 //     const response = await fetch(urlApi, options);
@@ -46,7 +59,7 @@ const options = {
 
 async function songs(){
     try {
-        const response = await fetch(`${API}/tracks/top`, options)
+        const response = await fetch(`${API}/tracks/top`, optionsMain)
         const tracks = await response.json();
         let count = 1
         const listSongs = tracks?.tracks
@@ -69,7 +82,7 @@ async function songs(){
 async function albums(){
     
     try {
-        const response = await fetch(`${API}/albums/top`, options)
+        const response = await fetch(`${API}/albums/top`, optionsMain)
         const albums = await response.json();
         let count = 1
         const listAlbums = albums?.albums
@@ -91,6 +104,43 @@ async function albums(){
     }
 
 }
+
+// async function artistsNames(artistId){
+//     const responseArtistsNames = await fetch(`${APIartists}${artistId}`, optionsArtists)
+//     const dataArtists = await responseArtistsNames.json();
+//     return dataArtists.name
+// }
+
+async function artists(){
+    try {
+        const response = await fetch(`${API}/artists/top`, optionsMain)
+        const artists = await response.json();
+        const listArtists = artists.artists
+
+        let top10Artists = listArtists.map(artist => artist.id).slice(0,10)
+
+        let nameTop10Artists = top10Artists.join(',');
+
+        const top10ArtistsNames = await fetch(`${APIartists}${nameTop10Artists}`, optionsArtists)
+        const nameTop10ArtistsNames = await top10ArtistsNames.json();
+
+
+        let count = 1
+        let templateArtists = `${nameTop10ArtistsNames.artists.map(artist => 
+            
+            `<div class="artists_card">
+                <a href="${artist.uri}" target="_blank">
+                <p class="artist_name">${count++}. ${artist.name} </p>
+                <img src="${artist.images[0].url}"/>
+                </a>
+            </div>`).slice(0,10).join("")
+            
+        }`
+        artistsContainer.innerHTML = templateArtists
+    } catch (error) {
+        console.log(error)
+    }
+}
 function executeSongs(){
     return new Promise(resolve => setTimeout(
         songs, 1000)
@@ -98,12 +148,18 @@ function executeSongs(){
 }
 function executeAlbums() {
     return new Promise(resolve => setTimeout(
-        albums, 5000)
+        albums, 3000)
+    )
+}
+function executeArtists() {
+    return new Promise(resolve => setTimeout(
+        artists, 5000)
     )
 }
 async function create(){ 
     executeSongs()
     executeAlbums()
+    executeArtists()
     
 }
 create()
